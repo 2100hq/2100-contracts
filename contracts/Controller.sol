b3f44c4f53dfaea64221a364112a459160931e6d
Brett <brett@citizenhex.com>
2019-07-17 10:45:28 -0400

    Controller updated to set more checks on deposit and withdraw, set minimum deposit

--- contracts/Controller.sol
+++ contracts/Controller.sol
@@ -9,6 +9,8 @@ import './classes/Creator.sol';
 contract Controller is Claimer, Minter, Creator{
     using SafeMath for uint256;
 
+    uint256 public minDeposit = 10**16; // 0.01 DAI
+
     event Deposit(address indexed account, uint256 amount, uint256 balance);
     event Withdraw(address indexed account, uint256 amount, uint256 balance);
     event Owner(address indexed owner);
@@ -22,6 +24,11 @@ contract Controller is Claimer, Minter, Creator{
 
     modifier onlyOwner(){
         assert(owner == address(0) || owner == msg.sender);
+        _;
+    }
+
+    modifier notOwner(){
+        require(owner != msg.sender, '2100: Owner cannot call this function');
         _;
     }
 
@@ -43,18 +50,27 @@ contract Controller is Claimer, Minter, Creator{
         return true;
     }
 
-    function deposit(uint256 amount) public returns (bool) {
-        amount = amount - amount.mod(10**16); // only accept pennies or greater
-        require(amount > 0, "Amount too small to deposit");
-        assert(DAI.transferFrom(msg.sender, address(this), amount));
+    function deposit(uint256 _amount) public notOwner returns (bool) {
+        uint256 amount = floor(_amount); // cut of greater than 2 decimal places
+
+        require(amount > 0, "2100: Amount too small to deposit; Requires at least 0.01 DAI");
+        require(DAI.allowance(msg.sender, address(this)) >= amount, "2100: Controller does not have enough allowance granted");
+        require(DAI.balanceOf(msg.sender) >= amount, "2100: Sender does not have sufficient balance to deposit");
+        require(DAI.balanceOf(msg.sender) > 0, "2100: Sender balance cannot be zero");
+
+        require(DAI.transferFrom(msg.sender, address(this), amount), "2100: DAI transfer failed");
+
         _balances[msg.sender] = _balances[msg.sender].add(amount);
         emit Deposit(msg.sender, amount, _balances[msg.sender]);
         return true;
     }
 
-    function withdraw(uint amount) public returns (bool) {
-        assert(_balances[msg.sender] <= amount);
-        assert(DAI.transfer(msg.sender, amount));
+    function withdraw(uint amount) public notOwner returns (bool) {
+        require(_balances[msg.sender] >= amount, "2100: Sender does not have enough deposited to withdraw");
+        require(DAI.balanceOf(address(this) >= amount, "2100: Controller does not have enough balance to withdraw");
+
+        require(DAI.transfer(msg.sender, amount), "2100: Transfer from Controller to Sender failed");
+
         _balances[msg.sender] = _balances[msg.sender].sub(amount);
         emit Withdraw(msg.sender, amount, _balances[msg.sender]);
         return true;
@@ -63,4 +79,10 @@ contract Controller is Claimer, Minter, Creator{
     function balanceOf(address account) public view returns (uint){
         return _balances[account];
     }
+
+    function floor(uint256 amount) internal view returns(uint256){
+        return amount.sub(amount.mod(minDeposit));
+    }
+
+
 }
