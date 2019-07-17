cb742ea86168dd186de91665d73a4061ca93d040
Brett <brett@citizenhex.com>
2019-07-17 10:48:07 -0400

    prettier

--- migrations/3_init_2100.js
+++ migrations/3_init_2100.js
@@ -1,21 +1,20 @@
-const Controller = artifacts.require("Controller");
-const SafeMath = artifacts.require("./libraries/SafeMath");
-const ECDSA = artifacts.require("./libraries/ECDSA");
-const FakeDAI = artifacts.require("./test/FakeDAI")
-module.exports = async function(deployer, network, accounts) {
-    const dai = await FakeDAI.deployed()
+const Controller = artifacts.require('Controller')
+const SafeMath = artifacts.require('./libraries/SafeMath')
+const ECDSA = artifacts.require('./libraries/ECDSA')
+const DummyDAI = artifacts.require('./test/DummyDAI')
+module.exports = async function (deployer, network, accounts) {
+  const dai = await DummyDAI.deployed()
 
-    await deployer.deploy(SafeMath);
-    await deployer.link(SafeMath, Controller);
-    await deployer.deploy(ECDSA);
-    await deployer.link(ECDSA, Controller);
+  await deployer.deploy(SafeMath)
+  await deployer.link(SafeMath, Controller)
+  await deployer.deploy(ECDSA)
+  await deployer.link(ECDSA, Controller)
 
-    await deployer.deploy(Controller, dai.address)
+  await deployer.deploy(Controller, dai.address)
 
-    const controller = await Controller.deployed()
+  const controller = await Controller.deployed()
 
-    console.log();
-    console.log("CONTROLLER ADDRESS", controller.address);
-    console.log("owner", await controller.owner())
-
-};
+  console.log()
+  console.log('CONTROLLER ADDRESS', controller.address)
+  console.log('owner', await controller.owner())
+}
