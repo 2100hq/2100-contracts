196ac6502451666e810725f16abb48c59c9c12c2
Brett <brett@citizenhex.com>
2019-07-18 11:03:25 -0400

    controller migration reads dummy address only in dev

--- migrations/3_init_2100.js
+++ migrations/3_init_2100.js
@@ -2,15 +2,22 @@ const Controller = artifacts.require('Controller')
 const SafeMath = artifacts.require('./libraries/SafeMath')
 const ECDSA = artifacts.require('./libraries/ECDSA')
 const DummyDAI = artifacts.require('./test/DummyDAI')
-module.exports = async function (deployer, network, accounts) {
-  const dai = await DummyDAI.deployed()
 
-  await deployer.deploy(SafeMath)
+async function getDAIAddress (network) {
+  if (/ganache|dev|test|artax/.test(network)) {
+    return (await DummyDAI.deployed()).address
+  }
+  throw new Error('Public Network DAI address not implemented')
+}
+
+module.exports = async function (deployer, network, accounts) {
+  const daiAddress = await getDAIAddress(network)
+
   await deployer.link(SafeMath, Controller)
   await deployer.deploy(ECDSA)
   await deployer.link(ECDSA, Controller)
 
-  await deployer.deploy(Controller, dai.address)
+  await deployer.deploy(Controller, daiAddress)
 
   const controller = await Controller.deployed()
 
