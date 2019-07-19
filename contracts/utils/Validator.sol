70bbec9fa90c0c5ebba3f972e17f1038f1e9fe7c
Brett <brett@citizenhex.com>
2019-07-19 10:45:56 -0400

    minor Validator error message changes

--- contracts/utils/Validator.sol
+++ contracts/utils/Validator.sol
@@ -7,15 +7,14 @@ contract Validator is Ownable {
     mapping (bytes32 => uint) public messageIdToBlockNumber;
 
     modifier onlyUnseenMessage(bytes32 messageId){
-      assert(seenMessage(messageId) == false);
+      require(seenMessage(messageId) == false, '2100: this message has aleady been process');
       _;
     }
 
     modifier onlyValidSignature(address signer, bytes32 hash, uint8 v, bytes32 r, bytes32 s){
-      require(signer == owner, 'signer not owner');
       require(signer != address(0), 'signer is 0');
       require(owner != address(0), 'owner is 0');
-      require(isValidSignature(signer, hash, v,r,s), '2100: signature is invalid');
+      require(isValidSignature(signer, hash, v,r,s), '2100: invalid signature');
       _;
     }
 
@@ -30,10 +29,10 @@ contract Validator is Ownable {
 
     function isValidSignature(address signer, bytes32 hash, uint8 v, bytes32 r, bytes32 s) public pure returns (bool)
     {
-        return signer == erecover(hash, v,r,s);
+        return signer == recoverEthSignedMessage(hash, v,r,s);
     }
 
-    function erecover(bytes32 hash, uint8 v, bytes32 r, bytes32 s) public pure returns(address){
+    function recoverEthSignedMessage(bytes32 hash, uint8 v, bytes32 r, bytes32 s) public pure returns(address){
       return ecrecover(keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash)), v, r, s);
     }
 }
